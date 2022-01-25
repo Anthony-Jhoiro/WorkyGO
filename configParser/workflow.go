1952a080c96a60f655aee4e5cb343b57479d8fe6
anthony <quereantho@gmail.com>
2022-01-25 14:19:58 +0100

    Clean unused files

--- configParser/workflow.go
+++ configParser/workflow.go
@@ -2,7 +2,6 @@ package configParser
 
 import (
 	"Workflow/workflow"
-	"fmt"
 )
 
 type Runner struct {
@@ -11,67 +10,4 @@ type Runner struct {
 	Maintainer  string
 	Arguments   string
 	Workflow    *workflow.Workflow
-}
-
-func NewStepDocker(f StepDockerFormat) *StepDocker {
-	return &StepDocker{
-		Id:        fmt.Sprintf("step-%s", f.Name),
-		Name:      f.Name,
-		Image:     f.Image,
-		Workdir:   f.Workdir,
-		Commands:  f.Commands,
-		DependsOn: f.DependsOn,
-		Persist:   f.Persist,
-	}
-}
-
-func BuildWorkflow(template WorkflowFileTemplate, metadata WorkflowMetadataTemplate) (*Runner, error) {
-
-	var runner Runner
-
-	runner.Name = metadata.Name
-	runner.Description = metadata.Description
-	runner.Maintainer = metadata.Maintainer
-
-	steps := make([]*workflow.Step, 0, len(template.Workflow.Steps))
-
-	for _, stepTemplate := range template.Workflow.Steps {
-		step, err := MapDictToStep(stepTemplate)
-		if err != nil {
-			return nil, err
-		}
-		// TODO : manage other steps
-		dockerStepFormat := step.(StepDockerFormat)
-		dockerStep := NewStepDocker(dockerStepFormat)
-		s := dockerStep.ToWorkFlowStep()
-
-		// Manage requirements
-		if len(dockerStep.DependsOn) != 0 {
-			for _, dependencyName := range dockerStep.DependsOn {
-				dependency := findDependency(steps, dependencyName)
-				if dependency == nil {
-					return nil, fmt.Errorf("fail to find dependency %s", dependencyName)
-				}
-
-				s.AddRequirement(dependency)
-			}
-		} else {
-			// TODO : Make it depends on a placeholder step
-		}
-
-		steps = append(steps, s)
-
-	}
-	runner.Workflow = workflow.NewWorkflow(steps[0], steps)
-
-	return &runner, nil
-}
-
-func findDependency(dependencies []*workflow.Step, dependencyName string) *workflow.Step {
-	for _, dependency := range dependencies {
-		if dependency.GetName() == dependencyName {
-			return dependency
-		}
-	}
-	return nil
 }
